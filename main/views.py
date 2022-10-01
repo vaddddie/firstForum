30de91985a8b7294fa383eeb621053954d56d2c4
vaddddie <v_bykovski@mail.ru>
2022-10-01 22:04:09 +0300

    First commit

---
+++ main/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views import View
+
+
+class index(View):
+    template_name = 'main/baseIndex.html'
+
+    context = {
+
+    }
+
+    def post(self, request, *args, **kwargs):
+        return render(request, self.template_name, self.context)
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, self.context)
+
+
